c5c6c14b05bd1e52e1ee825a887a223c62b59326
meat <g.chiang68@gmail.com>
2021-05-07 17:38:36 -0400

    updates persistence models and began linear regression

--- persistence.py
+++ persistence.py
@@ -1,19 +1,23 @@
+import pandas as pd 
 import matplotlib.pyplot as plt
-import pandas as pd #this is how I usually import pandas
-import matplotlib
 
 
 
-location = r'C:\Users\gchia\Desktop\597DE\solar_201502_201512_clean.csv'
-df = pd.read_csv(location, parse_dates=True, index_col=1)
+df = pd.read_csv('solar_small.csv', index_col=['timestamp'])
 df = df.drop(columns=['UTC'])
-dfcopy = df.shift()
-dfcopy = dfcopy.drop([0])
-print(dfcopy)
+
+dfcopy = df.copy().shift()
+df = df.drop(index=['2/2/2015 15:25'])
+dfcopy = dfcopy.drop(index=['2/2/2015 15:25'])
+differences = dfcopy.subtract(df)
+mape_df = abs(differences) / df
+#mape_df = mape_df.add(0, fill_value = 0)
+RMSE = ((differences ** 2).mean() * 0.5).mean()
+MAE = abs(differences).mean().mean()
+MAPE = mape_df.mean()
 
 
-#for index in df:
-    #if index != 'UTC':
-        #print(df[index])
+print(MAPE)
+
 #df.plot()
 #plt.show()
